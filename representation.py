a0cd085f4a7918767b90c7981a816af98153ea16
Daimy Van loo <daimyvanloo@sp-byods-145-109-44-192.wireless.uva.nl>
2024-06-04 15:21:38 +0200

    add basic attributes to classes and some init methods

--- representation.py
+++ representation.py
@@ -1,16 +1,23 @@
 class Subject:
     
     def __init__(self, name : str) -> None:
-        pass
+        self.name = name
 
 class Room:
-    pass
+
+    def __init__(self, room_number : str, capacity : str) -> None:
+        self.room_number = room_number
+        self.capacity = capacity
 
 class Activity:
     pass
 
 class Student:
-    pass
+    
+    def __init__(self, name : str, number : str, subject_names = list[str]) -> None:
+        self.name = name
+        self.student_number = number
+        self.subjects = []
         
 class Day:
     pass
