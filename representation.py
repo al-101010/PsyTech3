0a1d841f2adac6d8c91c8a95d0a5daadf489635d
Daimy Van loo <daimyvanloo@sp-byods-145-109-44-192.wireless.uva.nl>
2024-06-04 11:30:38 +0200

    add python file for representation

---
+++ representation.py
@@ -0,0 +1,16 @@
+class Subject:
+    
+    def __init__(self, name : str) -> None:
+        pass
+
+class Room:
+    pass
+
+class Activity:
+    pass
+
+class Student:
+    pass
+        
+class Day:
+    pass
