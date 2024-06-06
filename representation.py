c1467ef3bdfb08bf5089a2785d57fc4f42d1a0c0
Daimy Van loo <daimyvanloo@sp-byods-145-109-10-26.wireless.uva.nl>
2024-06-06 12:19:42 +0200

    add Student class

--- representation.py
+++ representation.py
@@ -1,7 +1,32 @@
+class Student:
+    
+    def __init__(self, name : str, number : str, subject_names = set[str]) -> None:
+        self.name = name
+        self.student_number = number
+        self.subject_names = subject_names
+        self.subjects = set()
+
+    def add_subjects(self, all_subjects : set):
+        """
+        Add a student's subjects to the set of subjects as a Subject class.
+        Also removes NA values.
+        """
+        for subject in all_subjects:
+            if subject.name in self.subject_names:
+                self.subjects.add(subject)
+
+
 class Subject:
     
-    def __init__(self, name : str) -> None:
+    def __init__(self, name : str, students : list[Student]) -> None:
         self.name = name
+
+    def add_activities(self):
+        pass
+
+class Activity:
+    def __init__(self, name : str, max_students : str) -> None:
+        pass
 
 class Room:
 
@@ -9,15 +34,9 @@ class Room:
         self.room_number = room_number
         self.capacity = capacity
 
-class Activity:
-    pass
 
-class Student:
-    
-    def __init__(self, name : str, number : str, subject_names = list[str]) -> None:
-        self.name = name
-        self.student_number = number
-        self.subjects = []
+
+
         
 class Day:
     pass
