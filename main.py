b194ebbb4f8e2d2991593397bec25b612db83faa
Daimy Van loo <daimyvanloo@MacBook-Air-van-Daimy.local>
2024-06-10 20:51:02 +0200

    remove print statement

--- main.py
+++ main.py
@@ -35,5 +35,3 @@ if __name__ == "__main__":
 
     print(get_output(test_schedule.students, 'data/random_output.csv'))
 
-    # for activity in test_schedule.activities:
-    #      print(activity.capacity, len(activity.students))
