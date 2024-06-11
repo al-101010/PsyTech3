bd37bd7d1000800d22f0799ce2d62a7c67c8fa8a
Daimy Van loo <daimyvanloo@sp-byods-145-109-25-172.wireless.uva.nl>
2024-06-11 09:42:51 +0200

    add comments to random algorithm

--- code/algorithms/random_alg.py
+++ code/algorithms/random_alg.py
@@ -8,40 +8,58 @@ class Random:
         self.schedule_students()
 
     def schedule_courses(self):
+        """
+        Schedule all activities on random days, timeslots and rooms.
+        """
+        # empty list for archive of all room-slots
         archive = []
 
-        #TO DO: add set of all combinations
+        # add all room-slots to archive
         for room in self.schedule.rooms:
             for day in room.days:
                 for time in room.timeslots:
                     archive.append((room, day, time))
 
+        # loop over all activities
         for activity in self.schedule.activities:  
+
+            # pick random room-slot to schedule this activity
             room_slot = random.choice(archive)  
             room = room_slot[0]
             day = room_slot[1]
             time = room_slot[2]
-
             activity.schedule(room, day, time)
+
+            # remove room-slot from archive
             archive.remove(room_slot)
 
     def schedule_students(self):
+        """
+        Schedule all students in random activities for the courses they follow.
+        """
+        # loop over all students and their courses
         for student in self.schedule.students:
             for course in student.courses:
                 for activity_type, activities in course.activities.items():
+
+                    # if activity is not a lecture, pick a random group
                     if activity_type != 'h':
                         activity = random.choice(activities)
 
+                        # pick a new random group while current group is at full capacity
                         while len(activity.students) == activity.capacity:
                             activity = random.choice(activities)
 
+                        # add students and activities together
                         student.activities.add(activity)
                         activity.students.add(student)
                     else:
+                        # schedule student to all lectures
                         for activity in activities:
                             student.activities.add(activity)
                             activity.students.add(student)
             
+            # update the student's personal schedule attribute
             student.personal_schedule()
 
             # calculate malus_points
