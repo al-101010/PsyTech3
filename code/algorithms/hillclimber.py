17c1690e6203e1156d8171eb1b36b9a1d8c246b3
Daantje de Laaf <daantje@Daantjes-Air.fritz.box>
2024-06-13 00:39:04 +0200

    expanded on improve_schedule function + added plot_graph func

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -1,10 +1,13 @@
 from algorithm import Algorithm
+import copy
+import matplotlib.pyplot as plt
 
 class Hillclimber(Algorithm):
     
     def __init__(self, schedule):
         super.__init__(schedule)
-
+        self.schedule = schedule
+        self.maluspoint_stats = []
 
     def improve_schedule(self, iters=10):
         """
@@ -21,12 +24,70 @@ class Hillclimber(Algorithm):
 
         # output final schedule
 
-    
+        previous_schedule = copy.deepcopy(self.schedule)
+        early_stopping_limit = 1000
+        early_stopping_counter = 0
+
+        # run through all iterations
+        for i in range(iters):
+
+            # check whether stagnates
+            if early_stopping_limit == early_stopping_counter:
+                print("stopping early due to a stagnation of improvements")
+                self.final_schedule = self.schedule
+                return
+            
+            # make random change to schedule
+            self.mutate_schedule()
+
+            # compute maluspoints for previous and current state 
+            previous_maluspoints = previous_schedule.get_total_maluspoints()
+            new_maluspoints = self.schedule.get_total_maluspoints()
+
+            # if improvement, reset counter and add number of maluspoint of new schedule to stats
+            if new_maluspoints <= previous_maluspoints:
+                early_stopping_counter = 0
+                self.statistics.append(new_maluspoints)
+
+            # else, increase counter and revert changes to schedule
+            else:
+                early_stopping_counter += 1
+                self.schedule = previous_schedule
+                self.statistics.append(previous_maluspoints)
+
+            # update "previous schedule"
+            previous_schedule = copy.deepcopy(self.schedule)
+        
+        # update final information in parent class
+        self.final_schedule = self.schedule
+        self.final_maluspoints = self.schedule.get_total_maluspoints()
+
+    def plot_graph(self, save=False):
+        """
+        plot maluspoints as a function of number of iterations (for hillclimber)
+        ACTUALLY SEEMED GOOD TO ADD THESE TO EACH ALGORITHM SEPARATELY, AS THE PLOT 
+        WILL DIFFER BETWEEN ALGORITHMS (E.G., RANDOM AND HILLCLIMBER)
+        """
+        iters = len(self.maluspoint_stats)
+        plt.plot(range(1, iters + 1), self.maluspoint_stats)
+        plt.xlabel('iteration')
+        plt.ylabel('maluspoints')
+        plt.title(f"number of iterations: {iters} & minimum maluspoints: {min(self.maluspoint_stats)}")
+
+        if save:
+            plt.savefig('../data/hillclimber_cost.png')
+
+        plt.show()
+
+
     # IDEA: switch roomslots instead of activities?
+            # maybe also keep these methods in the base algorithm class? as we'll prob need them for other algorithms
     def switch_course(self):
         """
         Switches the room, day, and timeslot of two random activities.
         """
+
+
         # loop until switch done successfully:
 
             # define two activities randomly that are candidates for switching
