50079fe47d33ab95d6367fa4c2e091a3f1f37783
Daantje de Laaf <daantje@Daantjes-Air.fritz.box>
2024-06-12 16:33:51 +0200

    added an (incomplete) function to mutate schedule

--- code/algorithms/algorithm.py
+++ code/algorithms/algorithm.py
@@ -22,3 +22,21 @@ class Algorithm:
         # switch the activities to the other roomslot
         random_roomslot1[0].schedule[random_roomslot1[1]][random_roomslot1[2]] = activity_2
         random_roomslot2[0].schedule[random_roomslot2[1]][random_roomslot2[2]] = activity_1
+
+    def mutate_schedule(self):
+        """
+        Mutate current schedule/timetable with a random action
+        INCOMPLETE -> need to add more ways to alter the schedule
+        = Useful when we have multiple ways of altering the schedule (as we don't want to make every
+        single alteration at ones)
+        """
+
+        schedule = self.schedule 
+
+        chance = random.random()
+
+        # if chance < 0.5:
+        #     switch activities/roomslots
+        # elif 0.5 <= chance < 0.8:
+        #     move student to other practical
+        #     or redistribute all students
