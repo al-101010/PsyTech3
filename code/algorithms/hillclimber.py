1fc0a9c9a6d29327a9fb2c2ccd3bfba874dd5ea8
Alexandra Genis <alexandra@Alexandras-MacBook-Pro-2.local>
2024-06-12 08:15:55 +0200

    hillclimber pseudocode proposal

---
+++ code/algorithms/hillclimber.py
@@ -0,0 +1,20 @@
+from .random_alg import Random
+
+# make class Hillclimber(takes a random schedule?)
+# self.final_schedule = get a random schedule
+# self.final_maluspoints = get maluspoints of random schedule
+
+
+# method to get the final schedule
+
+# define nr iterations or threshold
+# loop for x iterations or until threshold is reached
+    # temp_schedule = replace a random activity in schedule to different slot (call method below)
+    # temp_maluspoints = calculate maluspoints for temp schedule
+    # if maluspoints are < than final_maluspoints
+        # make this schedule the final schedule
+        # make this schedule's maluspoints final maluspoints
+
+# output final schedule and maluspoints
+
+# define method to replace room for activity  
