5719d38c81e1954b9a4374c756d0507f360a230e
al-101010 <contact.alexandragenis@gmail.com>
2024-06-13 12:02:20 +0200

    removed change rooms because already in algorithm

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -79,31 +79,3 @@ class Hillclimber(Algorithm):
             plt.savefig('../data/hillclimber_cost.png')
 
         plt.show()
-
-
-    # IDEA: switch roomslots instead of activities?
-            # maybe also keep these methods in the base algorithm class? as we'll prob need them for other algorithms
-    def switch_course(self):
-        """
-        Switches the room, day, and timeslot of two random activities.
-        """
-
-
-        # loop until switch done successfully:
-
-            # define two activities randomly that are candidates for switching
-            # course1 = random choice from schedule.courses
-            # course2 = random choice from schedule.courses
-
-            # activity1 = random choice from course1 activities
-            # activity2 = random choice from course2 activities
-
-            # if activity2.room > students number in activity1:
-                # assign room, day, timeslot of activity1 to activity2
-                # assign room, day, timeslot of activity2 to activity1
-                # stop looping
-
-        # update rooms with new schedule
-        # update students with new schedule
-        # update anywhere else where necessary
-        # return new schedule
