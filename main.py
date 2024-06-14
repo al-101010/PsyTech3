8814b77145e73f91bcc66e8270705077768a1f03
Daimy van Loo <daimyvanloo@gmail.com>
2024-06-14 20:47:09 +0200

    small variable change

--- main.py
+++ main.py
@@ -33,40 +33,40 @@ if __name__ == "__main__":
     test_schedule = Schedule('data/studenten_en_vakken.csv', 'data/vakken.csv', 'data/zalen.csv')
 
     # --------------------- RANDOM -----------------------------------------
-    # create schedule using random algorithm
-    random_schedule = Random(test_schedule)
+    # # create schedule using random algorithm
+    # random_schedule = Random(test_schedule)
 
-    # calculate malus points
-    maluspoints = random_schedule.schedule.get_total_maluspoints()
-    print(f"This schedule resulted in {maluspoints} maluspoints.")
-    print(f"Evening room usage: {random_schedule.schedule.get_evening_room_maluspoints()}")
-    get_output(random_schedule.schedule.students, 'data/random_output.csv')
+    # # calculate malus points
+    # maluspoints = random_schedule.schedule.get_total_maluspoints()
+    # print(f"This schedule resulted in {maluspoints} maluspoints.")
+    # print(f"Evening room usage: {random_schedule.schedule.get_evening_room_maluspoints()}")
+    # get_output(random_schedule.schedule.students, 'data/random_output.csv')
 
 
-    # --------------------- HILLCLIMBER -------------------------------------
-    hillclimber_schedule = Hillclimber(test_schedule)
-    hillclimber_schedule.run(500)
+    # # --------------------- HILLCLIMBER -------------------------------------
+    # hillclimber_schedule = Hillclimber(test_schedule)
+    # hillclimber_schedule.run(500)
 
-    hillclimber_schedule.plot_graph('data/hillclimber_cost.png', title='Hillclimber Algorithm', save=False)
-    get_output(hillclimber_schedule.schedule.students, 'data/hillclimber_output.csv')
+    # hillclimber_schedule.plot_graph('data/hillclimber_cost.png', title='Hillclimber Algorithm', save=False)
+    # get_output(hillclimber_schedule.schedule.students, 'data/hillclimber_output.csv')
 
 
     # --------------------- PLANT PROPAGATION ----------------------------------
     plantprop = PlantProp(test_schedule, early_stopping_limit=300)
-    PlantProp.run(5000)
+    plantprop.run(5000)
 
     plantprop.plot_graph('data/plantprop_cost.png', title='PlanProp Algorithm', save=False)
     get_output(plantprop.schedule.students, 'data/plantprop_output.csv')
 
 
-    # --------------------- SIMULATED ANNEALING --------------------------------
-    # create annealing schedule
-    simulated_annealing = SimulatedAnnealing(test_schedule, 500)
-    simulated_annealing.run(2000)
+    # # --------------------- SIMULATED ANNEALING --------------------------------
+    # # create annealing schedule
+    # simulated_annealing = SimulatedAnnealing(test_schedule, 500)
+    # simulated_annealing.run(2000)
 
-    simulated_annealing.plot_graph('data/simulated_annealing_plot.png', title='Simulated Annealing Algorithm', save=False)
-    print(get_output(simulated_annealing.schedule.students, 'data/simulated_annealing.csv'))
-    print(simulated_annealing.maluspoints)
+    # simulated_annealing.plot_graph('data/simulated_annealing_plot.png', title='Simulated Annealing Algorithm', save=False)
+    # print(get_output(simulated_annealing.schedule.students, 'data/simulated_annealing.csv'))
+    # print(simulated_annealing.maluspoints)
 
 
     
