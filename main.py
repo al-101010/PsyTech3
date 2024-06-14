ee1123f4180be01716e4d1c6fdb25aac24e49d1a
Daimy Van loo <daimyvanloo@MacBook-Air-van-Daimy.local>
2024-06-14 09:55:48 +0200

    test switch students method

--- main.py
+++ main.py
@@ -41,13 +41,12 @@ if __name__ == "__main__":
     start_time = time.time()
 
     hillclimber_schedule = Hillclimber(test_schedule)
-    print(get_output(hillclimber_schedule.schedule.students, 'data/hillclimber_test_output.csv'))
-
     hillclimber_schedule.run(200)
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
     hillclimber_schedule.plot_graph(save=False)
     print(get_output(hillclimber_schedule.schedule.students, 'data/hillclimber_output.csv'))
+
     
 
