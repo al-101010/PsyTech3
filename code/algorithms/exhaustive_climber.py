3726de474f478fe98b4e1cafee6554fff7dcd9a0
al-101010 <contact.alexandragenis@gmail.com>
2024-06-15 09:28:27 +0200

    hillclimber that fills all available rooms (heuristics module carcass)

---
+++ code/algorithms/exhaustive_climber.py
@@ -0,0 +1,27 @@
+from .hillclimber import Hillclimber
+
+# increase recursion limit for deepcopies
+sys.setrecursionlimit(10**6)
+
+class ExhaustiveClimber(Hillclimber):
+    """ 
+    Inherits from Hillclimber. 
+    Difference: Uses all free slots in course schedule (exhausts schedule space). 
+
+    TODOs:
+
+    - implement add_extra_activity in Algorithm
+    - test 
+    """
+
+    def __init__(self, empty_schedule : Schedule):
+        super().__init__(empty_schedule)
+        
+        # fill all free slots in schedule 
+        self.exhaust_schedule()    
+    
+    def exhaust_schedule(self):
+        # loops over all still vacant room slots in schedule until none free 
+            # picks a random activity and splits it (use below function i think)     
+            # self.add_extra_activity()
+   
