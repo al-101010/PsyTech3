8afdc71a215382ca131031f9b1de3a66b857986b
Daantje de Laaf <daantje@sp-byods-145-109-21-119.wireless.uva.nl>
2024-06-24 15:31:27 +0200

    added optional experiment to argparse

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from code.algorithms.random_alg import Random
 from code.algorithms.hillclimber import Hillclimber
 from code.algorithms.simulated_annealing import SimulatedAnnealing, ReheatSimulatedAnnealing
 from code.algorithms.plant_prop import PlantProp
-from code.algorithms.heuristics_hillclimber import HeuristicsHillclimber, ProblematicActivityClimber, ProblematicStudentsClimber, MutationProbabilityClimber, IncreaseMutationsClimber
+from code.algorithms.heuristics_hillclimber import ProblematicActivityClimber, ProblematicStudentsClimber, MutationProbabilityClimber, IncreaseMutationsClimber
 
 from experiments.hillclimber import hillclimber_experiment
 from experiments.simulated_annealing import simulated_annealing_experiment
@@ -35,7 +35,7 @@ def get_output(students : list, output : str):
         return schedule
 
 
-def main(algorithm, output_file_name, iterations, starting_temperature, version):
+def main(algorithm, output_csv_name, output_png_name, experiment, iterations, starting_temperature, version):
     test_schedule = Schedule('data/studenten_en_vakken.csv', 'data/vakken.csv', 'data/zalen.csv')
 
     if algorithm == 'random':
@@ -44,7 +44,7 @@ def main(algorithm, output_file_name, iterations, starting_temperature, version)
 
         # display malus points
         random_schedule.display_maluspoint_division('Random')
-        get_output(random_schedule.schedule.students, 'data/random_output.csv')
+        get_output(random_schedule.schedule.students, output_csv_name)
 
 
     if algorithm == 'hillclimber':
@@ -53,19 +53,20 @@ def main(algorithm, output_file_name, iterations, starting_temperature, version)
         hillclimber_schedule.run(iterations)
         hillclimber_schedule.display_maluspoints_division('Hillclimber')
 
-        hillclimber_schedule.plot_graph('data/hillclimber_cost.png', title='Hillclimber Algorithm', save=True)
-        get_output(hillclimber_schedule.schedule.students, 'data/hillclimber_output.csv')
+        hillclimber_schedule.plot_graph(output_png_name, title='Hillclimber Algorithm', save=True)
+        get_output(hillclimber_schedule.schedule.students, output_csv_name)
 
-        hillclimber_experiment.hillclimb_all_averages(test_schedule)
-        hillclimber_experiment.hillclimber_ratios_plot()
-        hillclimber_experiment.hillclimber_ratios_plot_zoom()
-        hillclimber_experiment.read_in_schedules()
-        hillclimber_experiment.timed_hillclimber_runs(test_schedule, Hillclimber) 
-        
-        # comparison of two hillclimbers 
-        # hillclimber_experiment.hillclimb(test_schedule, algorithm=Hillclimber, name='Hillclimber')
-        # hillclimber_experiment.hillclimb(test_schedule, algorithm=TargetedHillclimber, name='TargetedClimber')
-        # hillclimber_experiment.compare_hillclimbers("results/hillclimber/Hillclimber30_iter1000.csv", "results/hillclimber/TargetedClimber_30_iter1000.csv")
+        if experiment:
+            hillclimber_experiment.hillclimb_all_averages(test_schedule)
+            hillclimber_experiment.hillclimber_ratios_plot()
+            hillclimber_experiment.hillclimber_ratios_plot_zoom()
+            hillclimber_experiment.read_in_schedules()
+            hillclimber_experiment.timed_hillclimber_runs(test_schedule, Hillclimber) 
+            
+            # comparison of two hillclimbers 
+            # hillclimber_experiment.hillclimb(test_schedule, algorithm=Hillclimber, name='Hillclimber')
+            # hillclimber_experiment.hillclimb(test_schedule, algorithm=TargetedHillclimber, name='TargetedClimber')
+            # hillclimber_experiment.compare_hillclimbers("results/hillclimber/Hillclimber30_iter1000.csv", "results/hillclimber/TargetedClimber_30_iter1000.csv")
 
 
     if algorithm == 'plantprop':
@@ -73,8 +74,8 @@ def main(algorithm, output_file_name, iterations, starting_temperature, version)
         plantprop.run(iterations)
         plantprop.display_maluspoints_division('Plantprop')
 
-        plantprop.plot_graph('data/plantprop_cost.png', title='PlantProp Algorithm', save=True)
-        get_output(plantprop.schedule.students, 'data/plantprop_output.csv')
+        plantprop.plot_graph(output_png_name, title='PlantProp Algorithm', save=True)
+        get_output(plantprop.schedule.students, output_csv_name)
 
 
     if algorithm == 'simulated annealing':
@@ -84,14 +85,14 @@ def main(algorithm, output_file_name, iterations, starting_temperature, version)
             simulated_annealing.run(iterations)
             simulated_annealing.display_all_maluspoints('Simulated Annealing')
 
-            simulated_annealing.plot_graph('data/simulated_annealing_plot.png', title='Simulated Annealing Algorithm', save=False)
-            print(get_output(simulated_annealing.schedule.students, 'data/boltzexp_simulated_annealing.csv'))
+            simulated_annealing.plot_graph(output_png_name, title='Simulated Annealing Algorithm', save=False)
+            get_output(simulated_annealing.schedule.students, output_csv_name)
             
-
-            simulated_annealing_experiment.simal_all_averages(test_schedule)
-            simulated_annealing_experiment.simal_all_averages_plot()
-            simulated_annealing_experiment.simal_temp_comparisons(test_schedule)
-            simulated_annealing_experiment.simal_temp_comparisons_plot()
+            if experiment:
+                simulated_annealing_experiment.simal_all_averages(test_schedule)
+                simulated_annealing_experiment.simal_all_averages_plot()
+                simulated_annealing_experiment.simal_temp_comparisons(test_schedule)
+                simulated_annealing_experiment.simal_temp_comparisons_plot()
 
         if version == 'reheated':
             # create schedule
@@ -99,24 +100,13 @@ def main(algorithm, output_file_name, iterations, starting_temperature, version)
             reheat_simulated_annealing.run(iterations)
             reheat_simulated_annealing.display_all_maluspoints('Reheat Simulated Annealing')
 
-            reheat_simulated_annealing.plot_graph('data/boltzexp_reheat_simulated_annealing_plot.png', title='Reheat Simulated Annealing Algorithm', save=True)
-            print(get_output(reheat_simulated_annealing.schedule.students, 'data/boltzexp_reheat_simulated_annealing.csv'))
+            reheat_simulated_annealing.plot_graph(output_png_name, title='Reheat Simulated Annealing Algorithm', save=True)
+            get_output(reheat_simulated_annealing.schedule.students, output_csv_name)
             print(reheat_simulated_annealing.maluspoints)
-
-            reheating_experiment.reheating_averages(test_schedule)
-            reheating_experiment.reheating_plot()
-
-    # --------------------- HEURISTIC CLIMBER --------------------------------
-    # # create busy first schedule 
-    # heuristic_climb = HeuristicsHillclimber(test_schedule)
-    # heuristic_climb.run(20000)
-    # heuristic_climb.display_all_maluspoints('Hillclimber: Pick students with most maluspoints')
-    
-    # # get results 
-    # heuristic_climb.plot_graph('data/hillclimber_most_maluspoints_students_plot.png', title='Students with Most Maluspoints Heuristic', save=True)
-    # print(get_output(heuristic_climb.schedule.students, 'data/Hillclimber_most_maluspoints_students.csv'))
-    # print(heuristic_climb.maluspoints)
-
+            
+            if experiment:
+                reheating_experiment.reheating_averages(test_schedule)
+                reheating_experiment.reheating_plot()
 
     
 if __name__ == "__main__":
@@ -125,7 +115,9 @@ if __name__ == "__main__":
 
     # Adding arguments
     parser.add_argument("algorithm", help = "algorithm to run")
-    parser.add_argument("output", help = "output file (csv)")
+    parser.add_argument("output_csv", help = "output file (csv)")
+    parser.add_argument("output_png", help = "output plot (png)")
+    parser.add_argument("-e", "--experiment", type=bool, default = False, help = "experiment (default: False)")
     parser.add_argument("-i", "--iterations", type=int, default = 20000, help="iterations (default: 1930)")
     parser.add_argument("-t", "--starting_temperature",   type=int, default = 50, help="starting temperature (default: 50)")
     parser.add_argument("-v", "--version", type=str, default='normal', help="algorithm version (default: normal)")
@@ -134,4 +126,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Run main with provide arguments
-    main(args.algorithm, args.output, args.iterations, args.starting_temperature, args.version)
+    main(args.algorithm, args.output_csv, args.output_png, args.experiment, args.iterations, args.starting_temperature, args.version)
