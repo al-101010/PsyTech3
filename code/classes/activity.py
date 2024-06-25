bf6773b79fe5d2d704ade1b0b4df0faa11db78db
Daimy van Loo <daimyvanloo@gmail.com>
2024-06-25 14:50:17 +0200

    style

--- code/classes/activity.py
+++ code/classes/activity.py
@@ -23,14 +23,26 @@ class Activity:
         room.schedule[day][time] = self 
 
     def reset_maluspoints(self):
+        """
+        Resets this activity's maluspoints to 0.
+        """
         self.maluspoints = 0
 
-    def is_full(self):
+    def is_full(self) -> bool:
+        """
+        Returns true if activity is at full capacity.
+        """
         return len(self.students) >= self.capacity
     
-    def is_tutorial_practical(self):
+    def is_tutorial_practical(self) -> bool:
+        """
+        Returns true if actvity is a tutorial or practical.
+        """
         return 'w' in self.name or 'p' in self.name
     
     def get_roomslot(self):
+        """
+        Returns the roomslot this activity is scheduled in.
+        """
         return self.room, self.day, self.time
         
