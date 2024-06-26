9613187abe7bb8e7e0fa888d9bde71f65330ca57
Daimy van Loo <daimyvanloo@gmail.com>
2024-06-26 15:03:53 +0200

    clean

--- experiments/random/random_experiment.py
+++ experiments/random/random_experiment.py
@@ -1,14 +1,10 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from code.classes.schedule import Schedule
-from code.algorithms.random_alg import Random
-
 def run_algorithm(algorithm, schedule, iterations):
     maluspoints = []
 
     for i in range(iterations):
-        print(i)
         random_schedule = algorithm(schedule)
         maluspoints.append(random_schedule.schedule.get_total_maluspoints())
 
@@ -22,6 +18,3 @@ def visualize_maluspoints_barplot(algorithm, schedule, iterations):
     plt.title('Distribution of maluspoints over randomly generated schedules')
     plt.savefig('../data/random_cost.png')
 
-
-schedule = Schedule('data/studenten_en_vakken.csv', 'data/vakken.csv', 'data/zalen.csv')
-visualize_maluspoints_barplot(Random, schedule, 10000)
